ba6cfb23f19344e53d03f779d7b540d05546719d
alevrakis <dalevrakes529@gmail.com>
2022-05-11 14:33:16 +0300

    Matlab Ex 6  completed

--- Problem6.m
+++ Problem6.m
@@ -8,23 +8,25 @@ m = ceil(2*s*log(n));
 %Non zero elements
 n_perm = randperm(n);
 s_ind = n_perm(1:s);
-non_zero_vals = rand(s,1);
+non_zero_vals = randn(s,1);
 
 %Sparse vector
 x_s = sparse(s_ind,1,non_zero_vals,n,1);
 
 %A,b generation
-A = rand(m,n);
+A = randn(m,n);
 b = A*x_s;
 
 %CVX solutions
-lambda = 10;
+lambda = 1;
 cvx_begin quiet
-    variables x1_opt(n)
+    variables x1_opt(n) 
     minimize ((1/2)*pow_pos(norm(A*x1_opt-b,2),2) + lambda*norm(x1_opt,1))
 cvx_end
 % norm(x_s - x1_opt)
-f1_optval = cvx_optval;
+% f1_optval = cvx_optval;
+f1 = @(x) (1/2)*norm(A*x-b,2)^2+lambda*norm(x,1);
+f1_optval = f1(x_s);
 
 cvx_begin quiet
     variables x2_opt(n)
@@ -52,4 +54,16 @@ hold on
 [x_est_FISTA, f1k_iter_FISTA, iter_FISTA] = FISTA(A, b, x_init, f1_optval, c, lambda);
 semilogy(1:iter_FISTA+1,f1k_iter_FISTA-f1_optval);
 
-legend({'$ISTA$','$FISTA$'},'Interpreter','latex');
+[x_est_VFISTA, f1k_iter_VFISTA, iter_VFISTA] = VFISTA(A, b, x_init, f1_optval, c, lambda);
+semilogy(1:iter_VFISTA+1,f1k_iter_VFISTA-f1_optval);
+legend({'$ISTA$','$FISTA$','$VFISTA$'},'Interpreter','latex');
+hold off
+
+f2 = figure;
+stairs(x_s)
+hold on
+stairs(x_est_ISTA)
+stairs(x_est_FISTA)
+stairs(x_est_VFISTA)
+hold off
+legend({'$x_s$','$x_{est} ISTA$','$x_{est} FISTA$','$x_{est} VFISTA$'},'Interpreter','latex');
---
+++ VFISTA.m
@@ -0,0 +1,45 @@
+function [x_est, f1k_iter, iter] = VFISTA(A, b, x_init, f_opt, c, lambda)
+    f1 = @(x) (1/2)*norm(A*x-b,2)^2+lambda*norm(x,1);
+    ATA = A'*A;
+    ATb = A'*b; 
+    subg_f = @(x) ATA*x-ATb;
+    
+    T = 10^8; %Time Horizon
+
+    x_k = x_init;
+    y_k = x_init;
+    
+    iter = 0;
+    L_f = norm(ATA,'fro');
+    sigma = min(svd(A*A').^2); %sigma-strongly convex
+    k = L_f/sigma;
+    
+    multiplier = (sqrt(k)-1)/(sqrt(k)+1);
+    %First iteration f_k subg
+    f1_k = f1(x_k);
+    subg_k = subg_f(y_k);
+    
+    %
+    f1k_iter = zeros(T,1);
+    f1k_iter(iter+1) = f1_k;
+    for i=1:T-1
+        if(f1_k <= c*f_opt)
+            break;
+        end
+        
+        new_x_k = Soft_Thresholding(y_k - (1/L_f)*subg_k, 1/L_f);
+
+        y_k = new_x_k  + multiplier*(new_x_k - x_k);
+        
+        x_k = new_x_k;
+        
+        %Next Iteration f_k subg
+        f1_k = f1(x_k);
+        subg_k = subg_f(y_k);
+        iter = iter + 1;
+        
+        f1k_iter(iter+1)=f1_k;
+    end
+    f1k_iter  = f1k_iter(1:iter+1);
+    x_est = x_k;
+end
